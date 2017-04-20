0c700ebdf40b943ea3485eaa83dc07438bd4d50d
Whitney Burrow <whitneyvelvet@gmail.com>
2017-04-20 12:50:35 -0500

    Editted svdTest description

--- R/svdTest.R
+++ R/svdTest.R
@@ -1,4 +1,8 @@
 #' SVD Test
+#' 
+#' The following performs a Hotelling T2 test on a high dimensional
+#' dataset that has first been reduced via the singular value
+#' decomposition.
 #'
 #' @param x Data set 1.
 #' @param y Data set 2.
