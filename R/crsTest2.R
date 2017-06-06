a56008e5f662b4cb7981396f571ce07453796e90
Whitney Burrow <whitneyvelvet@gmail.com>
2017-06-06 10:16:03 -0500

    update to crstest2

--- R/crsTest2.R
+++ R/crsTest2.R
@@ -13,35 +13,28 @@ crsTest2 <- function(x, y, k, B1 = 100) {
   n1 <- nrow(x)
   n2 <- nrow(y)
   p <- ncol(x)
-  clusters <- pearsonClusters(x, y)
+  clusters <- burrowClusters2(x, y)
   cols <- lapply(unique(clusters$cluster), function(i) {
     which(clusters$cluster == i)
   })
   res <- sapply(seq_along(cols), function(i) {
     clusterCols <- cols[[i]]
-    xSub <- as.data.frame(x[, clusterCols])
-    ySub <- as.data.frame(y[, clusterCols])
-    hotellingT2(xSub, ySub)
+    xSub <- x[, clusterCols]
+    ySub <- y[, clusterCols]
+    k <- min(floor((n1 + n2 - 2) / 2), ncol(xSub))
+    rsTest(xSub, ySub, B1, k = k)
   })
-  clusterResult <- mean(res)
-  rsResult <- rsTest(x, y)
-  print(mean(c(clusterResult, rsResult)))
+  mean(res)
 }
 
 #' @rdname crsTest
 #' @export
 burrowClusters2 <- function(x, y) {
-  browser()
-  df <- Reduce(f = rbind, list(x = x, y = y))
   p <- ncol(x)
-  n <- nrow(df) - 2
-  kc <- floor(2 * n / 3)
-  distances <- pearsonDistance(df)
-  clusterStart <- flashClust::hclust(distances)
-  allCuts <- cutree(clusterStart, k = 1:ncol(x))
-  means <- sapply(1:p, function(i) mean(table(allCuts[, i])))
-  k <- which(abs(means - kc) == min(abs(means - kc)))
-  cuts <- allCuts[, k]
+  n <- nrow(x) + nrow(y) - 2
+  distances <- dist(x, method = "euclidean")
+  clusterStart <- flashClust::hclust(distances, method = "complete")
+  cuts <- cutree(clusterStart, k = 4)
   clusters <- data.frame(variable = names(cuts),
                          cluster = as.character(cuts),
                          stringsAsFactors = FALSE)
