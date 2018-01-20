125e8fefa8ac7c2d81280cc83d3e30ab6e059197
Whitney Burrow <whitneyvelvet@gmail.com>
2018-01-20 14:57:49 -0600

    Add Bai Sarandasa

---
+++ R/bsTest.R
@@ -0,0 +1,21 @@
+#' Bai Sarandasa Test
+#'
+#' @param x Data set 1.
+#' @param y Data set 2.
+#'
+#' @return Bai Sarandasa statistic for different mean vectors.
+#' @export
+
+bsTest <- function(x, y) {
+  n1 <- nrow(x)
+  n2 <- nrow(y)
+  dbar <- colMeans(x) - colMeans(y)
+  sx <- cov(x)
+  sy <- cov(y)
+  s <- ((n1 - 1) * sx + (n2 - 1) * sy) / (n1 + n2)
+  trace <- sum(diag(s))
+  trace2 <- sum(diag(s^2))
+  num <- (1 / n1 + 1 / n2) ^ (-1) * t(dbar) * dbar - trace
+  den <- sqrt(2 * (trace2 - 1/n * trace^2))
+  num / den
+}
